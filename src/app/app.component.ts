fa1776f1fad369415be4269a9165a1bea6bd0c64
japhari <japhari.mbaru@tamisemi.go.tz>
2022-09-19 13:50:58 +0300

    default language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,20 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor(private primengConfig: PrimeNGConfig) {}
+  language = 'en';
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    public translate: TranslateService
+  ) {
+    this.language = localStorage.getItem('lang');
+    translate.addLangs(['en', 'nl', 'fr', 'sw', 'po']);
+    if (this.language) {
+      translate.setDefaultLang('en');
+    } else {
+      translate.setDefaultLang('en');
+    }
+    console.log(this.language);
+  }
   val: number | undefined;
   ngOnInit() {
     this.primengConfig.ripple = true;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { PrivateComponent } from './pages/layout/private/private.component';
@@ -9,8 +12,10 @@ import { NotFoundComponent } from './pages/layout/not-found/not-found.component'
 import { PublicComponent } from './pages/layout/public/public.component';
 import { AppRoutingModule } from './app.routing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -24,7 +29,14 @@ import { TranslateModule } from '@ngx-translate/core';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    TranslateModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en',
+    }),
     SharedModule,
     AppRoutingModule,
   ],
